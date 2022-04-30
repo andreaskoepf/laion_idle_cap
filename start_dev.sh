712d6459ae66ccfcfc11b2ed28beffba44fc8ae9
Andreas Koepf <andreas.koepf@xamla.com>
2022-04-30 19:36:20 +0200

    fix start_dev.sh script

--- start_dev.sh
+++ start_dev.sh
@@ -26,7 +26,7 @@ run_args=(
 )
 
 echo "Starting docker container attached in dev mode..."
-echo "File $SCRIPT_DIR/docker/c_h+f.py" (outside container) is mapped to /mnt/spirit/c_h/c_h+f.py (inside container)"
-echo "Use 'docker attach $containerName' or './attach.sh' to attach to the process output."
-echo "Container ID:"
+echo "File $SCRIPT_DIR/docker/c_h+f.py (outside container) is mapped to /mnt/spirit/c_h/c_h+f.py (inside container)"
+echo "CTRL-c to kill the script running in the docker container."
+echo "CTRL-p CTRL-q (key sequence) to detach (process continues detached in background)."
 docker ${run_args[@]}
