7e2f4246a1a9fcf988ee1562474b058fa0788b7a
Andreas Koepf <andreas.koepf@xamla.com>
2022-04-30 19:27:47 +0200

    add start_dev script to use script outside docker container

---
+++ start_dev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+imageId=laion_idle_cah:v0
+containerName=laion_cah
+SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+# define array for docker run call, allows to comment individual arguments
+run_args=(
+    run
+    -it                        # interactive, allocate a pseudo-TTY, detach
+    --rm                        # automatically remove the container when it exits
+    --net=host                  # use host network
+    --name=$containerName       # name of container
+
+    # To restrict GPU availability inside the docker container (e.g. to hide your display GPU) you can use:
+    # --gpus '"device=1,2,3"'
+    # see https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/user-guide.html
+    --gpus all                  # specify which GPUs to use
+
+    # mount external source file
+    --mount type=bind,source="$SCRIPT_DIR/docker/c_h+.py",target="/mnt/spirit/c_h/c_h+.py"
+
+    -w /mnt/spirit/c_h          # set working directory
+    --runtime nvidia            # use nvidia runtime
+    $imageId
+    python3 c_h+.py             # command to execute
+)
+
+echo "Starting docker container detached in background mode..."
+echo "File $SCRIPT_DIR/docker/c_h+.py" (outside container) is mapped to /mnt/spirit/c_h/c_h+.py (inside container)"
+echo "Use 'docker attach $containerName' or './attach.sh' to attach to the process output."
+echo "Container ID:"
+docker ${run_args[@]}
